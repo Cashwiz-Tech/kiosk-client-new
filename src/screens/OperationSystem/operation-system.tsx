b6dab5b89161744869e7a65ca1820206a4efb138
Anton Makarov <keymonka@gmail.com>
2025-02-19 14:52:34 +0200

    Initial functionality; WIP

--- src/screens/OperationSystem/operation-system.tsx
+++ src/screens/OperationSystem/operation-system.tsx
@@ -1,111 +1,88 @@
 import "./operation-system.css";
-import usd_eur from './../../assets/usd_eur.png'
-import change_icon_orange from './../../assets/change_icon_orange.png'
-import card_icon from './../../assets/card_icon.png'
-import smart_kiosk from './../../assets/smart_kiosk.png'
 import { useAppDispatch } from "store/store";
 import { setCurrentScreen } from "store/navigationSlice";
 import { Screens } from "types/Screens";
-import ICT from './../../assets/ICT-logo-blue 2.png'
-import coins from './../../assets/coins.png'
-import cash_icon from './../../assets/cash_icon.png' 
-import take_icon from './../../assets/take_icon.png' 
-import qrcode_icon from './../../assets/qrcode_icon.png'
-import left_arrow_orange from './../../assets/left_arrow_orange.png'
 import { ReactComponent as Arrow } from "./../../assets/arrow.svg"
-//import  Arrow  from "./../../assets/left.png"
-
-
-import usd_icon from './../../assets/united states.png'
-import euro_icon from './../../assets/euro_icon.png'
 import Header from "layouts/header/Header";
-import { useEffect, useState } from "react";
-import ErrorScrenLeftModal from "components/buying/error-modal-screen-left";
-import cancel_white from '../../assets/cancel_white.png'
+import { useState } from "react";
 import Button from "lib/button";
 import NumericKeypadSmall from "components/buying/numeric-keypad-small/numeric-keypad-small";
+import { adminLogin } from "api/auth/admin";
+import { useParams } from "react-router-dom";
+import { setAdminAuthToken } from "store/authSlice";
 
 const OperationSystem = () => {
-    const dispatch = useAppDispatch();
-    
-    const [showScreenError, setshowScreenError] = useState(false);
-    const [timeoutID, settimeoutID] = useState<any>();
-    const [Value, setValue] = useState<any>('');
-   
-    function cancel_caracter(){
-        let Value_temp = Value;
-        let str = Value_temp.substring(0, Value_temp.length - 1);
+  const params = useParams();
+  const dispatch = useAppDispatch();
 
-        setValue(str);
+  const [code, setCode] = useState<string>("");
+
+  function cancelCharacter() {
+    setCode(code.substring(0, code.length - 1));
+  }
+
+  async function validateCode() {
+    const partnerId = params.partnerId || "";
+    const token = await adminLogin({
+      partnerId,
+      code,
+    });
+
+    if (!token) {
+      dispatch(setCurrentScreen(Screens.CODE_ERR));
+      return;
     }
 
-    function validate_code(){
+    setAdminAuthToken(token);
+    dispatch(setCurrentScreen(Screens.OPERATION_SYSTEM_TABS));
+  }
 
-        if (Value=='111111') { 
-           dispatch(setCurrentScreen(Screens.OPERATION_SYSTEM_TABS));
-        } else {
-            dispatch(setCurrentScreen(Screens.CODE_ERR));
-        }
+  function handleChangeCode(digit: string) {
+    setCode(code + digit);
+  }
 
-    }
+  return (
+    <>
+      <Header show_logo={false}></Header>
+      <div className="OperationSystem">
+        <div className="WelcomeScreenSecond-first-part">
+          <div className="mid-title">
+            הכנס קוד
+          </div>
 
+          <input type="text" className="operation-code-input" value={code} onChange={(e) => setCode(e.target.value)} />
 
-    function setValuefunc(v:string){
-        let Value_temp = Value ;
-        Value_temp+=v; 
-        setValue(Value_temp);
-    }
+          <NumericKeypadSmall setValue={(v: any) => handleChangeCode(v)} cancel_caracter={cancelCharacter} />
 
+          <div className="buttons-container-currency-total">
+            <Button onClick={validateCode} style={{
+              width: '344px'
+            }}>
+              אישור
+              <Arrow />
+            </Button>
 
-    return (
-        <>
-            <Header show_logo={false}></Header>
-            <div className="OperationSystem">
-                <div className="WelcomeScreenSecond_first_part">
-                    <div className="mid_title">
-                        הכנס קוד
-                    </div>
-                    
-                    <input type="text" className="operation_code_input" value={Value} onChange={(e)=>setValue(e.target.value)}/>
-
-                 
-                    <NumericKeypadSmall  setValue={(v:any) => setValuefunc(v)} cancel_caracter={cancel_caracter}/> 
-
-
-                    <div className="buttons-container-currency-total">
-
-                    <Button onClick={()=>validate_code()}  style={{
-                        width:'344px'}}>
-                    אישור
-                    <Arrow/>
-                            
-                            </Button>
-
-                    <Button onClick={()=>dispatch(setCurrentScreen(Screens.WELCOME_SCREEN))}
-                        style={{
-                        width:'344px',
-                        borderColor: "#fff",
-                        }}
-                     
-                        type="outline"
-                    >
-                        <img
-                        src={"/chevron-right.svg"}
-                        className="button-icon"
-                        alt="right arrow"
-                        />
-                        ביטול
-                    </Button>
-                    </div>
-
-                </div>
-
-            </div>
-         
-        </>
-    );
-
+            <Button onClick={() => dispatch(setCurrentScreen(Screens.WELCOME_SCREEN))}
+              style={{
+                width: '344px',
+                borderColor: "#fff",
+              }}
+              type="outline"
+            >
+              <img
+                src={"/chevron-right.svg"}
+                className="button-icon"
+                alt="right arrow"
+              />
+              <span>
+                ביטול
+              </span>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OperationSystem;
-
--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -2,24 +2,32 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface AuthState {
   token: string | null;
+  adminToken: string | null;
 }
 
 const initialState: AuthState = {
   token: null,
+  adminToken: null,
 };
 
 export const authSlice = createSlice({
   name: "authSlice",
   initialState,
   reducers: {
-    setAuthData: (
+    setAuthToken: (
       state,
       action: PayloadAction<AuthState["token"]>
     ) => {
       state.token = action.payload;
     },
+    setAdminAuthToken: (
+      state,
+      action: PayloadAction<AuthState["adminToken"]>
+    ) => {
+      state.adminToken = action.payload;
+    },
   },
 });
 
-export const { setAuthData } = authSlice.actions;
+export const { setAuthToken, setAdminAuthToken } = authSlice.actions;
 export default authSlice.reducer;
